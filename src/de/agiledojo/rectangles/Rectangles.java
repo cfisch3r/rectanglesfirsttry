14e4c8866018ecbb95e914f9f3118cdf8a3f3375
Christian Fischer <cf@fisch3r.de>
2016-09-24 08:46:51 +0200

    green: rectangle with right neighbour

--- src/de/agiledojo/rectangles/Rectangles.java
+++ src/de/agiledojo/rectangles/Rectangles.java
@@ -6,7 +6,11 @@ public class Rectangles {
     public Rectangle outline(List<Rectangle> rectangles) {
         Rectangle outline = null;
         for (Rectangle rectangle: rectangles) {
-            outline = new Rectangle(rectangle.x,rectangle.y,rectangle.w,rectangle.h);
+              if  (outline == null) {
+                  outline =new Rectangle(rectangle.x,rectangle.y,rectangle.w,rectangle.h);
+              } else {
+                  outline.w = (rectangle.x + rectangle.w - outline.x);
+              }
         }
         return outline;
     }
