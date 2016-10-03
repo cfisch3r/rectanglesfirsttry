3efe682abbbdf39df52d877b7e4b8dbbc6c270f2
Christian Fischer <cf@fisch3r.de>
2016-10-03 15:52:40 +0200

    refactor

--- src/de/agiledojo/rectangles/RectanglesTest.java
+++ src/de/agiledojo/rectangles/RectanglesTest.java
@@ -3,7 +3,9 @@ package de.agiledojo.rectangles;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
@@ -12,6 +14,8 @@ public class RectanglesTest {
 
     private Rectangles rectangles;
 
+    private List<Rectangle> fixture = new ArrayList<>();
+
     @Before
     public void setUp() {
         rectangles = new Rectangles();
@@ -19,21 +23,25 @@ public class RectanglesTest {
 
     @Test
     public void outlineSingleRectangle() {
-        assertEquals(new Rectangle(1,3,4,10), rectangles.outline(
-                asList(new Rectangle(1,3,4,10))));
+        withRectangle(1,3,4,10).assertOutlineToBe(1,3,4,10);
     }
 
     @Test
     public void outlineRectangleWithLeftNeigbour() throws Exception {
-        assertEquals(new Rectangle(0,3,5,10), rectangles.outline(
-                asList(new Rectangle(1,3,4,10),new Rectangle(0,3,4,10))));
-
+        withRectangle(1,3,4,10).withRectangle(0,3,4,10).assertOutlineToBe(0,3,5,10);
     }
 
     @Test
     public void outlineRectangleWithRightNeigbour() throws Exception {
-        assertEquals(new Rectangle(1,3,5,10), rectangles.outline(
-                asList(new Rectangle(1,3,4,10),new Rectangle(2,3,4,10))));
+        withRectangle(1,3,4,10).withRectangle(2,3,4,10).assertOutlineToBe(1,3,5,10);
+    }
 
+    private RectanglesTest withRectangle(int x, int y, int w, int h) {
+        fixture.add(new Rectangle(x,y,w,h));
+        return this;
+    }
+
+    private void assertOutlineToBe(int x, int y, int w, int h) {
+        assertEquals(new Rectangle(x,y,w,h),rectangles.outline(fixture));
     }
 }
